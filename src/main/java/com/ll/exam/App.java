0114801f534774cabd6a7d5837c49266f7d42630
JI-NAMEUN <skadms1720@gmail.com>
2022-07-08 14:37:41 +0900

    등록 기증 구현 시작

--- src/main/java/com/ll/exam/App.java
+++ src/main/java/com/ll/exam/App.java
@@ -13,6 +13,13 @@ public class App {
             String cmd = sc.nextLine().trim();
 
             switch (cmd) {
+                case "등록":
+                    System.out.println("명언: ");
+                    String context = sc.nextLine().trim();
+                    System.out.println("작가: ");
+                    String author = sc.nextLine().trim();
+                    System.out.println("1번 명령이 등록되었습니다.\n");
+                    break;
                 case "종료":
                     break outer;
             }
